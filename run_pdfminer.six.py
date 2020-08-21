ca022cc9db0679e851ce82efdd660126e635898b
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-08-21 08:35:36 +0000

    Add pdfminer.six

---
+++ run_pdfminer.six.py
@@ -0,0 +1,27 @@
+"""
+Run pdfminer.six parser on sample PDFs.
+
+Install with `pip install pdfminer.six`.
+"""
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+
+output_string = StringIO()
+with open("pdfs/1412.6980.pdf", 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+
+with open("pdfminer.six.out", "w") as f:
+    f.write(output_string.getvalue())
